6accd74d2de8e791a9aac5a3a28683481a32f2c2
Maximiliano Wortman <maximiliano.wortman@openenglish.com>
2017-11-06 09:54:12 -0300

    Credit card validation

--- tusLibros/src/serviceImpl/CreditCardServiceImpl.java
+++ tusLibros/src/serviceImpl/CreditCardServiceImpl.java
@@ -1,14 +1,30 @@
 package serviceImpl;
 
+import static utils.Utils.checkArgument;
+
+import exceptions.InvalidArgumentException;
 import java.util.Date;
 import model.CreditCard;
 import service.CreditCardService;
 
 public class CreditCardServiceImpl implements CreditCardService {
 
+  public static final String FORMATO_DE_FECHA_INVALIDO = "Formato de fecha invalido";
+
   @Override
   public CreditCard verifyCrediteCard(String ccn, String cced, String cco, Date current) {
-    //TODO
+    checkArgument(cced.length() == 6, FORMATO_DE_FECHA_INVALIDO);
+    checkArgument(cco.length() <= 30, "Formato de owner invalido");
+    String monthString = cced.substring(0, 2);
+    String yearString = cced.substring(2, 6);
+    int month = 0, year = 0;
+    try {
+      month = Integer.valueOf(monthString);
+      year = Integer.valueOf(yearString);
+    } catch (Exception ex) {
+      throw new InvalidArgumentException(FORMATO_DE_FECHA_INVALIDO);
+    }
+
     return null;
   }
 }
