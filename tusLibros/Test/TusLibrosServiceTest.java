19a27e024df1419be80e793c7df7220059e8b229
Maximiliano Wortman <maximiliano.wortman@openenglish.com>
2017-11-06 17:34:26 -0300

    Test integration

--- tusLibros/Test/TusLibrosServiceTest.java
+++ tusLibros/Test/TusLibrosServiceTest.java
@@ -9,12 +9,30 @@ import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
 import static org.junit.Assert.assertTrue;
 
+import java.util.Date;
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
+import mock.MockFactory;
+import model.Cashier;
+import model.Client;
 import model.Sale;
+import modelImpl.CashierImpl;
+import modelImpl.ClientImpl;
 import org.junit.Before;
 import org.junit.Test;
+import service.CartService;
+import service.ClientService;
+import service.CreditCardService;
+import service.PurchaseService;
+import service.TimeService;
 import service.TusLibrosService;
+import serviceImpl.CartServiceImpl;
+import serviceImpl.ClientServiceImpl;
+import serviceImpl.CreditCardServiceImpl;
+import serviceImpl.PurchaseServiceImpl;
+import serviceImpl.TimeServiceImpl;
 import serviceImpl.TusLibrosServiceImpl;
 
 public class TusLibrosServiceTest {
@@ -55,6 +73,10 @@ public class TusLibrosServiceTest {
   @Test
   public void testCheckOut() {
     tusLibrosService.checkOut(1L, "12341234122341234", "012050", "John Doe");
+  }
+
+  @Test
+  public void testIntegration() {
   }
 
 }
