1321009e917e597893f4e3ea3ad9f2ad50f50e09
Maximiliano Wortman <maximiliano.wortman@openenglish.com>
2017-11-06 10:50:28 -0300

    tests

--- tusLibros/Test/CreditCardServiceTest.java
+++ tusLibros/Test/CreditCardServiceTest.java
@@ -15,14 +15,36 @@ public class CreditCardServiceTest {
 
   @Test
   public void testCreditCardValidation() {
-    creditCardService.verifyCrediteCard("1234123412341234", "122017", "John Doe", new Date());
+    creditCardService.verifyCrediteCard("1234123412341234", "122050", "John Doe", new Date());
   }
 
   @Test
   public void testCreditCardValidationWithInvalidFormat() {
     expectedException.expect(InvalidArgumentException.class);
     expectedException.expectMessage(CreditCardServiceImpl.FORMATO_DE_FECHA_INVALIDO);
-    creditCardService.verifyCrediteCard("1234123412341234", "92017", "John Doe", new Date());
+    creditCardService.verifyCrediteCard("1234123412341234", "92050", "John Doe", new Date());
+  }
+
+  @Test
+  public void testCreditCardValidationWithInvalidFormatNumberLetters() {
+    expectedException.expect(InvalidArgumentException.class);
+    expectedException.expectMessage(CreditCardServiceImpl.NUMERO_DE_TARJETA_INVALIDO);
+    creditCardService.verifyCrediteCard("asdasdas", "092050", "John Doe", new Date());
+  }
+
+  @Test
+  public void testCreditCardValidationWithInvalidFormatNumberLength() {
+    expectedException.expect(InvalidArgumentException.class);
+    expectedException.expectMessage(CreditCardServiceImpl.NUMERO_DE_TARJETA_INVALIDO);
+    creditCardService.verifyCrediteCard("123412341234123", "092050", "John Doe", new Date());
+  }
+
+  @Test
+  public void testCreditCardValidationWithInvalidFormatOwner() {
+    expectedException.expect(InvalidArgumentException.class);
+    expectedException.expectMessage(CreditCardServiceImpl.FORMATO_DE_OWNER_INVALIDO);
+    creditCardService.verifyCrediteCard("123412341234123", "092050",
+        "John Doe algo muy largo que no entra en el nombre", new Date());
   }
 
 }
