7192d39c72fe0b9f60545538a2b27c2ae41e081c
Maximiliano Wortman <maximiliano.wortman@openenglish.com>
2017-11-05 22:49:01 -0300

    cart service

--- tusLibros/src/serviceImpl/CartServiceImpl.java
+++ tusLibros/src/serviceImpl/CartServiceImpl.java
@@ -1,13 +1,32 @@
 package serviceImpl;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
 import model.Cart;
 import model.Client;
+import modelImpl.CartImpl;
 import service.CartService;
 
 public class CartServiceImpl implements CartService {
 
+  private Long nextId;
+
+  private Set<String> catalogueIsbn;
+
+  private Map<Long, Cart> carts;
+
+  public CartServiceImpl(Long firstId, Set<String> catalogueIsbn) {
+    this.catalogueIsbn = catalogueIsbn;
+    this.nextId = firstId;
+    this.carts = new HashMap<>();
+  }
+
   @Override
   public Cart createCart(Client client) {
-    return null;
+    Cart cart = new CartImpl(nextId, catalogueIsbn);
+    nextId++;
+    carts.put(client.id(), cart);
+    return cart;
   }
 }
