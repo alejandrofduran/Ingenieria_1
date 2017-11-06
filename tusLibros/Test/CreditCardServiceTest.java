62230de4abca596a5b4e56c1a527dea528bd0658
Maximiliano Wortman <maximiliano.wortman@openenglish.com>
2017-11-06 10:29:37 -0300

    Test

--- tusLibros/Test/CreditCardServiceTest.java
+++ tusLibros/Test/CreditCardServiceTest.java
@@ -1,3 +1,23 @@
+import exceptions.InvalidArgumentException;
+import java.util.Date;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+import service.CreditCardService;
+import serviceImpl.CreditCardServiceImpl;
+
 public class CreditCardServiceTest {
+
+  @Rule
+  public ExpectedException expectedException = ExpectedException.none();
+
+  private CreditCardService creditCardService = new CreditCardServiceImpl();
+
+  @Test
+  public void testCreditCardValidation() {
+    expectedException.expect(InvalidArgumentException.class);
+    expectedException.expectMessage(CreditCardServiceImpl.FORMATO_DE_FECHA_INVALIDO);
+    creditCardService.verifyCrediteCard("1234123412341234", "152017", "John Doe", new Date());
+  }
 
 }
